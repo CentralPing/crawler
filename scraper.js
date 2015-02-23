df0aef6fed6391ef8db01ff1868682f04bbd600e
JasonCust <jason.cust@gmail.com>
2015-02-23 18:04:57 -0500

    feature: added options for config and batch size for detail requests to limit concurrent requests

--- scraper.js
+++ scraper.js
@@ -16,6 +16,10 @@ module.exports.trimText = trimText;
 
 var options = module.exports.options = {
   $: cheerio.load,
+  nextPagePath: 'nextPage',
+  itemsPath: 'items',
+  srcUrlPath: 'srcUrl',
+  detailBatchLimit: 10,
   logger: {
     warn: console.warn,
     error: console.error
@@ -24,13 +28,13 @@ var options = module.exports.options = {
 
 function crawl(req, parser, detailParser) {
   return scrape(req, parser, detailParser).then(function (results) {
-    if (results.nextPage !== undefined) {
-      return crawl({url: results.nextPage}, parser, detailParser).then(function (items) {
-        return [].concat(results.items, items);
+    if (results[options.nextPagePath] !== undefined) {
+      return crawl({url: results[options.nextPagePath]}, parser, detailParser).then(function (items) {
+        return [].concat(results[options.itemsPath], items);
       });
     }
 
-    return results.items;
+    return results[options.itemsPath];
   });
 }
 
@@ -47,32 +51,38 @@ function scrape(req, parser, detailParser) {
 
     var results = parser(options.$(decodeBody(resp, body)));
 
-    if (detailParser === undefined) {
+    if (detailParser === undefined || !_.isArray(results[options.itemsPath])) {
       return deferred.resolve(results);
     }
 
-    return q.allSettled(results.items.map(function (item) {
-      if (item.srcUrl === undefined) { return {}; }
-
-      return scrape({url: item.srcUrl}, detailParser);
-    })).then(function (details) {
-      details.forEach(function (result, i) {
-        if (result.state === 'fulfilled') {
-          _.merge(results.items[i], result.value);
-        }
-        else {
-          options.logger.error(result.reason);
-        }
-      });
-
-      return deferred.resolve({
-        nextPage: results.nextPage,
-        items: results.items
-      });
+    return detailBatch(results[options.itemsPath], detailParser, 0).then(function () {
+      return deferred.resolve(results);
     });
   });
 
   return deferred.promise;
+}
+
+function detailBatch(items, parser, start) {
+  var end = start + options.detailBatchLimit;
+  var batch = items.slice(start, end);
+
+  return q.allSettled(batch.map(function (item) {
+    if (item[options.srcUrlPath] === undefined) { return {}; }
+
+    return scrape({url: item[options.srcUrlPath]}, parser);
+  })).then(function (details) {
+    details.forEach(function (result, i) {
+      if (result.state === 'fulfilled') {
+        _.merge(batch[i], result.value);
+      }
+      else {
+        options.logger.error(result.reason);
+      }
+    });
+
+    return (end < items.length) ? detailBatch(items, parser, end) : items;
+  });
 }
 
 function trimText($el) {
